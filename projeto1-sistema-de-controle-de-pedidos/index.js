2ea682c5351704d89a7b3916da9eb312bcfe763a
Robson <robson.filho14@aluno.ce.gov.br>
2026-08-29 14:33:25 -0300

    Add files via upload

---
+++ projeto1-sistema-de-controle-de-pedidos/index.js
@@ -0,0 +1,56 @@
+// CRIE SUA SOLUÇÃO ABAIXO ================
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+const preco = 3500
+const quantidade = 2
+const descontoPercentual = 10
+const valorPago = 7000
+const subtotal = preco * quantidade
+const valorDesconto = (subtotal * descontoPercentual) / 100
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+const resumo = `
+==================================================
+TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+==================================================
+
+Cliente: ${cliente}
+Cidade: ${cidade}
+
+Produto: ${produto}
+Categoria: ${categoria}
+Quantidade: ${quantidade}
+Preço unitário: R$ ${preco}
+
+Subtotal: R$ ${subtotal}
+Desconto: ${descontoPercentual}%
+Valor do desconto: R$ ${valorDesconto}
+Valor final: R$ ${valorFinal}
+
+Valor pago: R$ ${valorPago}
+Troco: R$ ${troco}
+
+Obrigado pela compra
+`
+console.log(resumo)
+
+// === FIM DO CÓDIGO =======================
+// === NÃO FAZER NADA ABAIXO DESSA LINHA ===
+module.exports = {
+  cliente,
+  cidade,
+  produto,
+  categoria,
+  preco,
+  quantidade,
+  descontoPercentual,
+  valorPago,
+  subtotal,
+  valorDesconto,
+  valorFinal,
+  troco,
+  resumo
+}
